bc738228bf1aa7ca77c7e0617c8ce076af0cf3b2
kiran <kiran@DESKTOP-KK6FE83>
2022-08-15 17:11:24 +0530

    Shopme : code refactor

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.shopme.admin.user;
+package com.shopme.admin.user.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,6 +22,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
+import com.shopme.admin.user.UserNotFoundException;
+import com.shopme.admin.user.UserService;
 import com.shopme.admin.user.export.UserCsvExporter;
 import com.shopme.admin.user.export.UserExcelExporter;
 import com.shopme.admin.user.export.UserPdfExporter;
@@ -69,7 +71,7 @@ public class UserController {
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		model.addAttribute("keyword", keyword);
 
-		return "users";
+		return "users/users";
 	}
 
 	// New user route
@@ -86,7 +88,7 @@ public class UserController {
 		model.addAttribute("listRoles", listRoles);
 		model.addAttribute("pageTitle", "Create New User");
 
-		return "user_form";
+		return "users/user_form";
 	}
 
 	@PostMapping("/users/save")
@@ -129,7 +131,7 @@ public class UserController {
 			model.addAttribute("user", user);
 			model.addAttribute("listRoles", listRoles);
 			model.addAttribute("pageTitle", "Edit User Id : " + id );
-			return "user_form";
+			return "users/user_form";
 
 		} catch (UserNotFoundException e) {
 
