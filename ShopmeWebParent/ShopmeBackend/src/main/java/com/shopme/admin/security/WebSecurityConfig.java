cf026f8e64f423cc77cad6dbe33b103583a0d579
kiran <kiran@DESKTOP-KK6FE83>
2022-08-15 16:08:55 +0530

    Shopme : implemented navigation option showed according with roles

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -49,7 +49,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().anyRequest().authenticated().and()
+		http.authorizeRequests()
+		.antMatchers("/users/**").hasAuthority("Admin")
+		.anyRequest().authenticated().and()
 		.formLogin().loginPage("/login").usernameParameter("email")
 		.permitAll().and().logout().permitAll()
 		.and()
